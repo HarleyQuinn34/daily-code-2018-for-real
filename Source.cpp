2920830dcdfb85e243d1483ddbb759ff4a3a1763
HarleyQuinn34 <35504117+HarleyQuinn34@users.noreply.github.com>
2018-10-18 13:00:50 -0600

    8 slot bool array

--- Source.cpp
+++ Source.cpp
@@ -1,16 +1,20 @@
-#include <iostream >
+#include <iostream>
 using namespace std;
 int main() {
-	int MyArray[12];
-	int Number = 0;
-	for (int j=0; j < 12; j++)
-		cin >> MyArray[j];
-	for (int i=0; i < 12; i++) {
-		if (MyArray[i] > Number) {
-			Number = MyArray[i];
-		}
+	int both=0 ;
+	bool arro[8];
+	bool arra[8];
+	cout<<"Enter your first set of 8 numbers"<<endl;
+	for (int j = 0; j < 8; j++)
+		cin >> arro[j];
+	cout << "Enter your second set of 8 numbers" << endl;
+	for (int i = 0; i < 8; i++)
+		cin >> arra[i];
 
-	}
-	cout << "The biggest number is " << Number << endl;
+	for (int j = 0; j < 8; j++) 
+		if (arro[j] == arra[j])
+			both++;
+	
+	cout<<"There are "<<both<<" numbers that are the same."<<endl;
 	system("pause");
 }
