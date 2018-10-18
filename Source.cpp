67589e685dc99a7f39089f7cda7e95add2737390
HarleyQuinn34 <35504117+HarleyQuinn34@users.noreply.github.com>
2018-10-18 14:38:04 -0600

    snowing function

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
+#include<ctime>
 using namespace std;
-int main() {
-	int phone[10];
-cout << "What is your number bb?" << endl;
-		
-	for (int i = 0; i < 10; i++) 
-		cin >> phone[i];
-	
 
-	for (int i = 0; i < 10; i++)
-		cout << phone[i];
+void snowflake(int a, int b);
+
+int main() {
+	srand(time(NULL));
+	int a = 90;
+	int b = 900000;
+
+
+
+
+
+	snowflake(a, b);
+
+
 	system("pause");
+}
+
+void snowflake(int a, int b) {
+	int num;
+	for (int i = 1; i < b; i++) {//"game loop" how long it runs
+		num = rand() % a + 1;
+
+		for (int t = 1; t <num; t++)//print a bunch of spaces
+			cout << " ";
+
+		cout << " * ";//and one flake
+	}
+
+
+
 }
