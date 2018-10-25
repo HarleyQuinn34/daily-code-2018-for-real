7caef6a1978a8d03e2098f365baa0bc214689205
HarleyQuinn34 <35504117+HarleyQuinn34@users.noreply.github.com>
2018-10-25 13:57:12 -0600

    roof square footage calculation

--- Source.cpp
+++ Source.cpp
@@ -1,210 +1,113 @@
-#include <stdio.h>
-#include <allegro5/allegro.h>
-#include <allegro5\allegro_image.h>
-#include<iostream>
+#include <iostream>
 using namespace std;
+///////////////declarations
+double hiprec(int c, int l, int a);
+double gabe( int l, int a);
+double hipsqr( int l, int a);
+double leanto( int l, int a);
+double man(int c, int l, int a);
 
 
-const float FPS = 60;
-const int SCREEN_W = 900;
-const int SCREEN_H = 500;
-const int BOUNCER_SIZE = 32;
-int worldWidth = 4128;
-int worldHeight = 3754;
-int cameraX = 0;
-int cameraY = 0;
-enum MYKEYS {
-	KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT
-};
+int main() {
+	//variables for functions
+	int c=0;
+	int a=0;
+	int l = 0;
+	//users input
+	char input;
+	//switch delcare/ name
+	int roof;
+	while (1) {
+		cout << "What kind of roof is it?" << endl;
+		cout << "(h) for hip roof rectangle bldg." << endl;
+		cout << "(g) for  gable roof." << endl;
+		cout << "(s) for hip roof square bldg." << endl;
+		cout << "(l) for lean to roof." << endl;
+		cout << "(m) for mansard roof." << endl;
+		cin >> input;
 
-int main(int argc, char **argv)
-{
-	ALLEGRO_DISPLAY *display = NULL;
-	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-	ALLEGRO_TIMER *timer = NULL;
-	ALLEGRO_BITMAP *bouncer = NULL;
-	ALLEGRO_BITMAP *map = NULL;
-	//float bouncer_x = SCREEN_W / 2.0 - BOUNCER_SIZE / 2.0;
-	//float bouncer_y = SCREEN_H / 2.0 - BOUNCER_SIZE / 2.0;
-
-	float bouncer_x = 2606 + SCREEN_W;
-	float bouncer_y = 574 + SCREEN_W;
-
-	bool key[4] = { false, false, false, false };
-	bool redraw = true;
-	bool doexit = false;
-	bool onEdge = false;
-
-
-	al_init();
-
-	al_init_image_addon();
-
-	al_install_keyboard();
-	map = al_load_bitmap("ff1.png");
-
-	timer = al_create_timer(1.0 / FPS);
-
-
-	display = al_create_display(SCREEN_W, SCREEN_H);
-
-
-	bouncer = al_create_bitmap(BOUNCER_SIZE, BOUNCER_SIZE);
-
-
-	al_set_target_bitmap(bouncer);
-
-	al_clear_to_color(al_map_rgb(255, 0, 255));
-
-	al_set_target_bitmap(al_get_backbuffer(display));
-
-	event_queue = al_create_event_queue();
-
-
-	al_register_event_source(event_queue, al_get_display_event_source(display));
-
-	al_register_event_source(event_queue, al_get_timer_event_source(timer));
-
-	al_register_event_source(event_queue, al_get_keyboard_event_source());
-
-	al_clear_to_color(al_map_rgb(0, 0, 0));
-
-	al_flip_display();
-
-	al_start_timer(timer);
-
-	while (!doexit)
-	{
-		ALLEGRO_EVENT ev;
-		al_wait_for_event(event_queue, &ev);
-
-		if (ev.type == ALLEGRO_EVENT_TIMER) {
-
-			cameraX = bouncer_x - SCREEN_W / 2;
-			cameraY = bouncer_y - SCREEN_H / 2;
-
-			if (key[KEY_UP] && bouncer_y < worldHeight + 32) {
-				bouncer_y -= 4.0;
-			}
-
-			if (key[KEY_DOWN] && bouncer_y < worldHeight - 32) {
-				bouncer_y += 4.0;
-			}
-
-			if (key[KEY_LEFT] && bouncer_x < worldWidth - 32) {
-				bouncer_x -= 4.0;
-			}
-
-			if (key[KEY_RIGHT] && bouncer_x < worldWidth - 32) {
-				bouncer_x += 4.0;
-			}
-
-			redraw = true;
-			cout << "onedge is " << onEdge << endl;
-			//left
-			if (cameraX < 0) {
-				cameraX = 0;
-				onEdge = true; 
-			}
-			else 
-				onEdge = false;
-
-
-			//top
-			if (cameraY < 0) {
-				cameraY = 0;
-				onEdge = true;
-			}
-			else
-				onEdge = false;
-
-
-			//right
-			if (cameraX > worldWidth - SCREEN_W) { 
-				cameraX = worldWidth - SCREEN_W;
-				onEdge = true;
-			}
-			else
-				onEdge = false;
-
-			//bottom
-			if (cameraY > worldHeight - SCREEN_H) {
-				cameraY = worldHeight - SCREEN_H;
-				onEdge = true;
-			}
-			else
-				onEdge = false;
-			}
-
-			else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-				break;
-			}
-			else if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
-				switch (ev.keyboard.keycode) {
-				case ALLEGRO_KEY_UP:
-					key[KEY_UP] = true;
-					break;
-
-				case ALLEGRO_KEY_DOWN:
-					key[KEY_DOWN] = true;
-					break;
-
-				case ALLEGRO_KEY_LEFT:
-					key[KEY_LEFT] = true;
-					break;
-
-				case ALLEGRO_KEY_RIGHT:
-					key[KEY_RIGHT] = true;
-					break;
-				}
-			}
-			else if (ev.type == ALLEGRO_EVENT_KEY_UP) {
-				switch (ev.keyboard.keycode) {
-				case ALLEGRO_KEY_UP:
-					key[KEY_UP] = false;
-					break;
-
-				case ALLEGRO_KEY_DOWN:
-					key[KEY_DOWN] = false;
-					break;
-
-				case ALLEGRO_KEY_LEFT:
-					key[KEY_LEFT] = false;
-					break;
-
-				case ALLEGRO_KEY_RIGHT:
-					key[KEY_RIGHT] = false;
-					break;
-
-				case ALLEGRO_KEY_ESCAPE:
-					doexit = true;
-					break;
-				}
-			}
-
-			if (redraw && al_is_event_queue_empty(event_queue)) {
-				redraw = false;
-
-				al_clear_to_color(al_map_rgb(0, 0, 0));
-
-				//this draws your map
-				al_draw_bitmap(map, -cameraX, -cameraY, 0);
-
-				//if you're not on the edge, draw the player in the middle!
-			if (onEdge == false)
-				al_draw_bitmap(bouncer, SCREEN_W / 2, SCREEN_H / 2, 0);
-			else//otherwise let him move!
-				al_draw_bitmap(bouncer, bouncer_x - cameraX, bouncer_y - cameraY, 0);
-
-
-				al_flip_display();
-			}
-		}
-
-		al_destroy_bitmap(bouncer);
-		al_destroy_timer(timer);
-		al_destroy_display(display);
-		al_destroy_event_queue(event_queue);
-
-		return 0;
+		//asking which roof the user has and then sending them to switch statement
+		if (input == 'h')
+			roof = 1;
+		else if (input == 'g')
+			roof = 2;
+		else if (input == 's')
+			roof = 3;
+		else if (input == 'l')
+			roof = 4;
+		else if (input == 'm')
+			roof = 5;
+		//switch statement
+		switch (roof) {
+		case 1:
+			cout << "Your roof square footage is " << hiprec(c, l, a) << endl;
+			break;
+		case 2:
+			cout << "Your roof square footage is " << gabe(l, a) << endl;
+			break;
+		case 3:
+			cout << "Your roof square footage is " << hipsqr(l, a) << endl;
+			break;
+		case 4:
+			cout << "Your roof square footage is " << leanto(l, a) << endl;
+			break;
+		case 5:
+			cout << "Your roof square footage is " << man(c, l, a) << endl;
+			break;
+		}//end of switch
 	}
+	system("pause");
+}//end of main
+///////////////////////////////function definitions
+double hiprec(int c, int l, int a) {
+	//double sum;
+	cout << "what is your c of the roof?" << endl;
+	cin >> c;
+	cout << "what is your length of roof?" << endl;
+	cin >> l;
+	cout << "what is your area of roof?" << endl;
+	cin >> a;
+	return (c + l)*a / 2;
+
+}
+double gabe( int l, int a) {
+	//double sum;
+
+	cout << "what is your length of roof?" << endl;
+	cin >> l;
+	cout << "what is your area of roof?" << endl;
+	cin >> a;
+	return 2*a*l;
+
+}
+double hipsqr(int l, int a) {
+	//double sum;
+
+	cout << "what is your length of roof?" << endl;
+	cin >> l;
+	cout << "what is your area of roof?" << endl;
+	cin >> a;
+	return  a*l/2;
+
+}
+double leanto(int l, int a) {
+	//double sum;
+
+	cout << "what is your length of roof?" << endl;
+	cin >> l;
+	cout << "what is your area of roof?" << endl;
+	cin >> a;
+	return  a*l;
+
+}
+double man(int c, int l, int a) {
+	//double sum;
+	cout << "what is your c of the roof?" << endl;
+	cin >> c;
+	cout << "what is your length of roof?" << endl;
+	cin >> l;
+	cout << "what is your area of roof?" << endl;
+	cin >> a;
+	return (a + l)*c / 2;
+
+}
