12986ddc8a68fca62c44546c42bc835df6a3a62e
HarleyQuinn34 <35504117+HarleyQuinn34@users.noreply.github.com>
2018-10-29 14:19:38 -0600

    guessing the right number

--- Source.cpp
+++ Source.cpp
@@ -1,113 +1,47 @@
+/*Isaiah Salinas 10/29/18
+Write a text based number-guessing game program. 
+The program picks a number between 1 and 100 and asks the user to guess what number it has chosen. 
+If the user guesses wrong, the program tells them whether they're too high or too low. 
+When the user guesses correctly, the program prints out a congratulatory message and tells them how many turns it took them.
+When the player finally wins, the program should ask them if they want to play again, and if not, quit the game.*/
 #include <iostream>
+#include <ctime>
 using namespace std;
-///////////////declarations
-double hiprec(int c, int l, int a);
-double gabe( int l, int a);
-double hipsqr( int l, int a);
-double leanto( int l, int a);
-double man(int c, int l, int a);
 
 
 int main() {
-	//variables for functions
-	int c=0;
-	int a=0;
-	int l = 0;
-	//users input
-	char input;
-	//switch delcare/ name
-	int roof;
-	while (1) {
-		cout << "What kind of roof is it?" << endl;
-		cout << "(h) for hip roof rectangle bldg." << endl;
-		cout << "(g) for  gable roof." << endl;
-		cout << "(s) for hip roof square bldg." << endl;
-		cout << "(l) for lean to roof." << endl;
-		cout << "(m) for mansard roof." << endl;
-		cin >> input;
+	int turn = 0;
+	int input=200;
+	char in = 'y';
+	srand(time(NULL));
+	int num;
+	
+	while (in != 'n') {//starts over when you guess right
+		num = rand() % 100 + 1;
 
-		//asking which roof the user has and then sending them to switch statement
-		if (input == 'h')
-			roof = 1;
-		else if (input == 'g')
-			roof = 2;
-		else if (input == 's')
-			roof = 3;
-		else if (input == 'l')
-			roof = 4;
-		else if (input == 'm')
-			roof = 5;
-		//switch statement
-		switch (roof) {
-		case 1:
-			cout << "Your roof square footage is " << hiprec(c, l, a) << endl;
-			break;
-		case 2:
-			cout << "Your roof square footage is " << gabe(l, a) << endl;
-			break;
-		case 3:
-			cout << "Your roof square footage is " << hipsqr(l, a) << endl;
-			break;
-		case 4:
-			cout << "Your roof square footage is " << leanto(l, a) << endl;
-			break;
-		case 5:
-			cout << "Your roof square footage is " << man(c, l, a) << endl;
-			break;
-		}//end of switch
-	}
-	system("pause");
-}//end of main
-///////////////////////////////function definitions
-double hiprec(int c, int l, int a) {
-	//double sum;
-	cout << "what is your c of the roof?" << endl;
-	cin >> c;
-	cout << "what is your length of roof?" << endl;
-	cin >> l;
-	cout << "what is your area of roof?" << endl;
-	cin >> a;
-	return (c + l)*a / 2;
+		while (num != input) { //keep going until your guess (input) equals the random number (num)
+			
+			cout << "You have guessed: " << turn <<" times."<< endl;
+			cout << "Guess arandom number between 1-100." << endl;
+			cin >> input;
+			if (input == num) {
+                turn++;
+				cout << "nice :)" << endl;
+				cout << "You have guessed in total: " << turn << " times." << endl;\
+				cout << "Do you want to play again?" << endl;
+				cout << "(y) for yes or (n) for no." << endl;
+				cin >> in;
+			}
+			else if (input < num) {
+			cout << "The number you have guessed is too low." << endl;
+			 turn++ ;
+		}
+			else if (input > num) {
+				cout << "The number you have guessed is too high." << endl;
+				 turn++;
+			}
+		}//end turn
 
-}
-double gabe( int l, int a) {
-	//double sum;
-
-	cout << "what is your length of roof?" << endl;
-	cin >> l;
-	cout << "what is your area of roof?" << endl;
-	cin >> a;
-	return 2*a*l;
-
-}
-double hipsqr(int l, int a) {
-	//double sum;
-
-	cout << "what is your length of roof?" << endl;
-	cin >> l;
-	cout << "what is your area of roof?" << endl;
-	cin >> a;
-	return  a*l/2;
-
-}
-double leanto(int l, int a) {
-	//double sum;
-
-	cout << "what is your length of roof?" << endl;
-	cin >> l;
-	cout << "what is your area of roof?" << endl;
-	cin >> a;
-	return  a*l;
-
-}
-double man(int c, int l, int a) {
-	//double sum;
-	cout << "what is your c of the roof?" << endl;
-	cin >> c;
-	cout << "what is your length of roof?" << endl;
-	cin >> l;
-	cout << "what is your area of roof?" << endl;
-	cin >> a;
-	return (a + l)*c / 2;
-
+	}//end game
+	cout << "goodbye!" << endl;
 }
