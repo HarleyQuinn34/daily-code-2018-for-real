1afdebd533fdf0fc1c094e3ae8f435a890777b4a
HarleyQuinn34 <35504117+HarleyQuinn34@users.noreply.github.com>
2018-10-29 14:20:41 -0600

    putting numbers in order

--- Source.cpp
+++ Source.cpp
@@ -1,47 +1,35 @@
-/*Isaiah Salinas 10/29/18
-Write a text based number-guessing game program. 
-The program picks a number between 1 and 100 and asks the user to guess what number it has chosen. 
-If the user guesses wrong, the program tells them whether they're too high or too low. 
-When the user guesses correctly, the program prints out a congratulatory message and tells them how many turns it took them.
-When the player finally wins, the program should ask them if they want to play again, and if not, quit the game.*/
 #include <iostream>
-#include <ctime>
 using namespace std;
 
 
+
 int main() {
-	int turn = 0;
-	int input=200;
-	char in = 'y';
-	srand(time(NULL));
-	int num;
-	
-	while (in != 'n') {//starts over when you guess right
-		num = rand() % 100 + 1;
+	//variable declarations
+	int num[10];
+	int temp;
+	//load in 10 numbers from user into the array
+	for (int i = 0; i < 10; i++) {
+		cout << "In order for this to work you will need to give us 10 random numbers." << endl;
+		cin >> num[i];
+	}
 
-		while (num != input) { //keep going until your guess (input) equals the random number (num)
-			
-			cout << "You have guessed: " << turn <<" times."<< endl;
-			cout << "Guess arandom number between 1-100." << endl;
-			cin >> input;
-			if (input == num) {
-                turn++;
-				cout << "nice :)" << endl;
-				cout << "You have guessed in total: " << turn << " times." << endl;\
-				cout << "Do you want to play again?" << endl;
-				cout << "(y) for yes or (n) for no." << endl;
-				cin >> in;
+	//bubble sort!
+	for (int j = 0; j < 10; j++) {
+		for (int i = 0; i < 9; i++) {
+			if (num[i] > num[i + 1]) {
+				temp = num[i + 1];
+				num[i + 1] = num[i];
+				num[i] = temp;
 			}
-			else if (input < num) {
-			cout << "The number you have guessed is too low." << endl;
-			 turn++ ;
 		}
-			else if (input > num) {
-				cout << "The number you have guessed is too high." << endl;
-				 turn++;
-			}
-		}//end turn
+	}
 
-	}//end game
-	cout << "goodbye!" << endl;
+	//print out the final array
+	for (int i = 0; i < 10; i++) {
+		cout << num[i] << "  ";
+	}
+
+
+
+	system("pause");
 }
