2b5cc8b11044c446b81c49575b5bbbb20448854f
HarleyQuinn34 <35504117+HarleyQuinn34@users.noreply.github.com>
2018-11-27 12:48:18 -0700

    users score and critiquing the score

--- Source.cpp
+++ Source.cpp
@@ -1,31 +1,16 @@
 #include <iostream>
-#include <fstream>
 using namespace std;
-int num;
-int num2;
-int num3;
-
 int main() {
-	//output file
-	ofstream myfile;
-	myfile.open("save1.txt");
-
-	//input file
-	ifstream myfile2;
-	myfile2.open("save.txt");
-
-	myfile2 >> num;
-	//cout << "num is " << num << endl;
-	for (int i = 0; i < num; i++) {
-		cout << "loop " << num;
-		myfile2 >> num2;
-		myfile2 >> num3;
-
-
-		myfile << num2 + num3<<" ";
-		myfile << num2 * num3 << endl;
+	int score;
+	while (1) {
+		cout << "what is your score cuh?" << endl;
+		cin >> score;
+		if (score < 100)
+			cout << "yous a buster" << endl;
+		else if (score > 100 && score < 1000)
+			cout << "You aight cuh" << endl;
+		else if (score > 1000)
+			cout << "damn cuh, i see you " << endl;
+		system("pause");
 	}
-	myfile.close();
-	myfile2.close();
-	system("pause");
 }
