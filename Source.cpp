9fc3d15bacbb33cf177ac59dbe87855ec0d7d079
HarleyQuinn34 <35504117+HarleyQuinn34@users.noreply.github.com>
2018-11-29 13:03:28 -0700

    making boxes with file io

--- Source.cpp
+++ Source.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
+int num;
+int num2;
 int main() {
-	int score;
-	while (1) {
-		cout << "what is your score cuh?" << endl;
-		cin >> score;
-		if (score < 100)
-			cout << "yous a buster" << endl;
-		else if (score > 100 && score < 1000)
-			cout << "You aight cuh" << endl;
-		else if (score > 1000)
-			cout << "damn cuh, i see you " << endl;
-		system("pause");
+	//output file
+	ofstream myfile;
+	myfile.open("save1.txt");
+	//input file
+	ifstream myfile2;
+	myfile2.open("save.txt");
+	myfile2 >> num;
+	for (int i = 0; i < num; i++) {
+		myfile2 >> num2;
+		for (int m = 0; m < num2; m++){
+			for (int j = 0; j < num2; j++) {
+				myfile << "$";
+			}
+			myfile << endl;
 	}
 }
+		myfile.close();
+		myfile2.close();
+		system("pause");
+	}
